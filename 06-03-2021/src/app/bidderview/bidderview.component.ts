fd177e6e824d9c47e22b21b41b3b906f4bcf0f56
sainadh <79893612+sainadh13@users.noreply.github.com>
2021-03-06 21:01:00 +0530

    06-03-2021

---
+++ 06-03-2021/src/app/bidderview/bidderview.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidderview',
+  templateUrl: './bidderview.component.html',
+  styleUrls: ['./bidderview.component.css']
+})
+export class BidderviewComponent implements OnInit {
+
+  constructor() { }
+  viewbids() {
+
+  }
+  viewlands() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 06-03-2021/src/app/bidsview/bidsview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidsview',
+  templateUrl: './bidsview.component.html',
+  styleUrls: ['./bidsview.component.css']
+})
+export class BidsviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
