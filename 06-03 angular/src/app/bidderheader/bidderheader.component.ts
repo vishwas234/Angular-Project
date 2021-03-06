54ccf4a463447728f41172d67c1933b846ce29c5
Bala Chandrudu Bondigala <78945152+BalaChandrudu409@users.noreply.github.com>
2021-03-06 18:00:34 +0530

    06-03 angular

---
+++ 06-03 angular/src/app/bidderheader/bidderheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidderheader',
+  templateUrl: './bidderheader.component.html',
+  styleUrls: ['./bidderheader.component.css']
+})
+export class BidderheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 06-03 angular/src/app/viewlands/viewlands.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-viewlands',
+  templateUrl: './viewlands.component.html',
+  styleUrls: ['./viewlands.component.css']
+})
+export class ViewlandsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
